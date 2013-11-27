8bc225fbe5e34a0ecdcf429f4df828b8ff37cd54
developingidea <zalai.gabor@artinvent.hu>
2013-11-27 11:57:39 +0100

    scarf, footprint animation

--- js/animation.js
+++ js/animation.js
@@ -1,68 +1,13 @@
-var pagenumberone = $("#number-one"),
-    slidecontentone = $("#content-one"),
-
-  
+var scarf = $(".scarf"),
+    footprint1 = $(".footprint1"),
+    footprint2 = $(".footprint2"),
+    footprint3 = $(".footprint3"),  
     
-    tl = new TimelineMax({repeat:-1});
+    tl = new TimelineMax({repeat:0});
 
-
-tl.set(slidecontentone,  {autoAlpha:1})
- 
-  .from(slidecontentone, 1.2, {bottom:-100, autoAlpha:0}) 
-  .from(pagenumberone, 1, {bottom:-100, opacity: 0, autoAlpha:0}, "-=1.5")
-  
-
-
-$("#seek-slide1").click(function() {
-    tl.seek(1);
-});
-		  
-$("#seek-slide2").click(function() {
-		tl.seek(9);
-});
-
-$("#seek-slide3").click(function() {
-    tl.seek(17);
-});
-
-$("#seek-slide4").click(function() {
-    tl.seek(22);
-});
-
-$("#seek-slide5").click(function() {
-    tl.seek(27);
-});
-
-
-$("#seek-slide6").click(function() {
-    tl.seek(32);
-});
-
-$("#seek-slide7").click(function() {
-    tl.seek(41);
-});
-
-
-
-
-		
-$("#pause").click(function() {
-		tl.pause();
-});
-		
-$("#reverse").click(function() {
-		tl.reverse();
-});
-		
-$("#resume").click(function() {
-		tl.resume();	
-});
-		
-$("#restart").click(function() {
-		tl.restart();
-});
-
-$("#next").click(function() {
-    tl.next();
-});
-	
+tl.set(footprint1,  {autoAlpha:1})  
+  .to([footprint1, footprint2, footprint3, scarf], 0, {  opacity: 0, autoAlpha:1, delay: 0}) 
+  .to(footprint1, 1.8, {  opacity: 1, autoAlpha:1, delay: 2.5}) 
+  .to(footprint2, 1.8, {  opacity: 1, autoAlpha:1, delay: 0.5}) 
+  .to(footprint3, 1.8, {  opacity: 1, autoAlpha:1, delay: 0.5})
+  .to(scarf, 1.2, {  opacity: 1, autoAlpha:1, delay: 1})
