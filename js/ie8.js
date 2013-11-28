169ab92e9d95a5482c4058a1645983ae1b83ee07
developingidea <zalai.gabor@artinvent.hu>
2013-11-28 13:31:59 +0100

    ie8 anim fade fix

--- js/ie8.js
+++ js/ie8.js
@@ -1,13 +1,13 @@
 
 $(window).load(function() {
 
-$('h2').css({opacity:0}).delay(3150).animate({opacity:1,}, 2000,'easeOutSine');
-$('h1').css({opacity:0,}).delay(6750).animate({opacity:1,}, 1300,'easeOutSine');
+$('h2').css({opacity:0}).delay(7150).animate({opacity:1,}, 2000,'easeOutSine');
+$('h1').css({opacity:0,}).delay(10750).animate({opacity:1,}, 1300,'easeOutSine');
 
-$('.footprint1').css({opacity:0}).delay(2000).animate({opacity:1,}, 2000,'linear');
-$('.footprint2').css({opacity:0,}).delay(2500).animate({opacity:1,}, 1300,'linear');
-$('.footprint3').css({opacity:0,}).delay(3000).animate({opacity:1,}, 1300,'linear');
+$('.footprint1').css({opacity:0}).delay(5000).animate({opacity:1,}, 2000,'linear');
+$('.footprint2').css({opacity:0,}).delay(5000).animate({opacity:1,}, 1300,'linear');
+$('.footprint3').css({opacity:0,}).delay(9000).animate({opacity:1,}, 1300,'linear');
 
-$('.scarf').css({opacity:0,}).delay(5050).animate({opacity:1,}, 1300,'linear');
+$('.scarf').css({opacity:0,}).delay(11050).animate({opacity:1,}, 1300,'linear');
 
 });
